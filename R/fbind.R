65da7668c10029e259a1db4b1902add767f35881
Suh-Yuen Liang <syliang18@gmail.com>
2021-08-25 16:32:48 +0800

    my second commit

---
+++ R/fbind.R
@@ -0,0 +1,13 @@
+#' combined two list of factors into one
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+fbind <- function(a, b) {
+  factor(c(as.character(a), as.character(b)))
+}
